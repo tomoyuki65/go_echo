c96f0e5829ed36e3ddfca4e175a56977451587f8
tomoyuki65 <tomoyuki65@users.noreply.github.com>
2024-06-02 01:00:07 +0900

    init

---
+++ api/src/router/router.go
@@ -0,0 +1,19 @@
+// package router
+
+// import (
+//   // "github.com/gin-gonic/gin"
+// //   "go_v2/controller"
+//   // "go_v2/api/handlers"
+// )
+
+// func Init() {
+//   // routerの初期化
+//   router := gin.Default()
+
+//   // routerの設定
+//   router.GET("/", handlers.Index)
+//   router.GET("/test", handlers.Test)
+
+//   // routerを起動
+//   router.Run(":3010")
+// }
