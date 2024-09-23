cb8fab87d16523c2a6d1ed631f5ce0c47f2dd001
tomoyuki65 <tomoyuki65@users.noreply.github.com>
2024-09-23 23:14:24 +0900

    modify Dockerfile

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# 環境変数「EXECUTE_MIGRATION」の値で制御する
 if [ "$EXECUTE_MIGRATION" == "true" ]; then
   echo "マイグレーション実行"
   atlas migrate apply \
@@ -7,4 +8,5 @@ if [ "$EXECUTE_MIGRATION" == "true" ]; then
   --url "mysql://$MYSQL_USER:$MYSQL_PASSWORD@$MYSQL_HOST:3306/$MYSQL_DATABASE"
 fi
 
+# DockerfileのCMDを実行
 exec "$@"
